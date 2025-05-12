afd11d974ea84738afa2f12608de8d0f82a9915d
AbhishekSatpathy4848Alias <abhisheksatpathy2003@gmail.com>
2025-05-12 19:47:00 +0000

    minor server startup changes

--- backend/app.py
+++ backend/app.py
@@ -431,6 +431,6 @@ if __name__ == '__main__':
     background_thread.start()
     
     # Start the Flask application
-    app.run(debug=True, host='0.0.0.0', port=8000)
+    app.run(debug=False, host='127.0.0.1', port=8000)
 
 
